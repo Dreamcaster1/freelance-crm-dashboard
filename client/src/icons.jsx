173662c04e87275f73b6bbeb19cce60e89933658
Dreamcaster1 <maslovskyy.fb@gmail.com>
2026-06-06 02:24:15 +0100

    Refine design system

--- client/src/icons.jsx
+++ client/src/icons.jsx
@@ -4,7 +4,7 @@ const iconProps = {
   viewBox: '0 0 16 16',
   fill: 'none',
   stroke: 'currentColor',
-  strokeWidth: 1.5,
+  strokeWidth: 1.25,
   strokeLinecap: 'round',
   strokeLinejoin: 'round',
   'aria-hidden': true,
